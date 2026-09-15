445ba82223dfdc6c64b92dc47d49bc7e58b2b0ea
Andrew Hankinson <andrew.hankinson@gmail.com>
2026-09-15 12:36:35 +0000

    Exercise update

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -1,6 +1,6 @@
 # Write a program that prints the words "Hello Edirom" to the console.
-# Run it with `python exercises/ex1.py` in the terminal.
-# It must contain at least one function.
+# Run it with `python exercises/ex1.py` in the terminal, or with the "|>" button in the top-right.
+# Your solution must contain at least one function.
 
 
 if __name__ == "__main__":
